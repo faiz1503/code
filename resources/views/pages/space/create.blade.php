cb7d7c46207a4aa1d8efe153320b2e1ff81d18dc
faiz1503 <faiz16ti@mahasiswa.pcr.ac.id>
2020-08-22 19:56:03 +0700

    create space

--- resources/views/pages/space/create.blade.php
+++ resources/views/pages/space/create.blade.php
@@ -6,10 +6,10 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Submit My Space') }}</div>
+                <div class="card-header">Submit My Space</div>
 
                 <div class="card-body">
-                    {!!  Form::open(['route' => 'space.store', 'method' => 'post', 'files' => true]) !!}
+                   {!! Form::open(['route' => 'space.store', 'method' => 'post', 'files' => true]) !!}
                     <div class="form-group">
                         <label for="">Title</label>
                         {!! Form::text('title', null, ['class' => $errors->has('title') ? 'form-control is-invalid' : 'form-control']) !!}
@@ -45,7 +45,7 @@
                             </span>
                         @enderror
                     </div>
-                    <div class="here-maps">
+                    <div id="here-maps">
                         <label for="">Pin Location</label>
                         <div style="height: 500px" id="mapContainer"></div>
                     </div>
@@ -67,8 +67,32 @@
                             </span>
                         @enderror
                     </div>
+                    <div class="form-group increment">
+                        <label for="">Photo</label>
+                        <div class="input-group">
+                            <input type="file" name="photo[]" class="form-control">
+                            <div class="input-group-append">
+                                <button type="button" class="btn btn-outline-primary btn-add"><i class="fas fa-plus-square"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                    @if ($errors->has('photo'))
+                        <ul class="alert alert-danger">
+                            @foreach ($errors->get('photo') as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="clone invisible">
+                        <div class="input-group mt-2">
+                            <input type="file" name="photo[]" class="form-control">
+                            <div class="input-group-append">
+                                <button type="button" class="btn btn-outline-danger btn-remove"><i class="fas fa-minus-square"></i></button>
+                            </div>
+                        </div>
+                    </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
-                    {!! Form::close() !!}
+                   {!! Form::close() !!}
                 </div>
             </div>
         </div>
@@ -79,5 +103,14 @@
 @push('script')
     <script>
         window.action = "submit"
+        jQuery(document).ready(function () {
+            jQuery(".btn-add").click(function () {
+                let markup = jQuery(".invisible").html();
+                jQuery(".increment").append(markup);
+            });
+            jQuery("body").on("click", ".btn-remove", function () {
+                jQuery(this).parents(".input-group").remove();
+            })
+        })
     </script>
 @endpush
