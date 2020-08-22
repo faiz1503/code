0484f829ecb41f94b826aa41bbefbfc0b19aa1d5
faiz1503 <faiz16ti@mahasiswa.pcr.ac.id>
2020-08-22 19:56:10 +0700

    show space

--- resources/views/pages/space/index.blade.php
+++ resources/views/pages/space/index.blade.php
@@ -17,16 +17,16 @@
                     <h5 class="card-title">
                         {{ $space->title }}
                         @if ($space->user_id == Auth::user()->id)
-                        <form action="#">
+                        <form action="{{ route('space.destroy', $space->id) }}" method="POST">
                             @csrf @method('DELETE')
-                            <button type="submit" class="btn btn-sm btn-danger float-right">Delete</button>
+                            <button type="submit" class="btn btn-sm btn-danger float-right" onclick="return confirm('Are you sure?');">Delete</button>
                             <a href="{{ route('space.edit', $space->id) }}" class="btn btn-sm btn-info float-right text-white">Edit</a>
                         </form>
                         @endif
                     </h5>
                     <h6 class="card-subtitle">{{ $space->address }}</h6>
                     <p class="card-text">{{ $space->description }}</p>
-                    <a href="#" class="card-link">Direction</a>
+                    <a href="#" onclick="openDirection({{ $space->latitude }}, {{ $space->longitude }}, {{ $space->id }})" class="card-link">Direction</a>
                 </div>
             </div>
             @endforeach
